9515144cf0111b748fbddc7b33d582f762c49b72
conrad123 <outerhaven@live.it>
2017-06-28 22:59:59 +0200

    websocket server and client added

---
+++ websocketserver.py
@@ -0,0 +1,67 @@
+import json
+from tornado import httpserver,websocket,ioloop,web
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler, Stream
+
+# ---------------------------------------------------------------------------
+# Handler per il websocket server [open, on_message, on_close, check_origin]
+class WSHandler(websocket.WebSocketHandler):
+
+    def open(self):
+        print 'new connection'
+      
+    def on_message(self, message):
+        if message=='Start':
+            access_token = "479635577-RggBMFaIFh9HkbC5JMwNj1KsnWbaYvJusrzNEYvT"
+            access_token_secret = "RD58EwiM6uxbNnq1cOxDxr06emV0HuimoGDgw40zFPYIX"
+            consumer_key = "gS7DkeN3SlL6DtG4XR3e5rWnb"
+            consumer_secret = "cPCT9xJhQYJh2gRaAqu5K2V7bZERkfv3t2TcZpadswO9sRzoxN"
+
+            auth = OAuthHandler(consumer_key, consumer_secret)
+            auth.set_access_token(access_token, access_token_secret)
+            stream = Stream(auth, TweetsListener(self)) 
+            stream.filter(track=['#NowPlaying'],async=True)
+ 
+    def on_close(self):
+        print 'connection closed'
+ 
+    def check_origin(self, origin):
+        return True
+        
+# URI dell'handler
+application = web.Application([
+    (r'/ws', WSHandler),
+])
+# ---------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------
+# Ascoltatore di tweets [on_data, on_error, ws e' il WSHandler]
+class TweetsListener(StreamListener):
+
+    def __init__(self,ws):
+        self.ws = ws
+
+    def on_data(self, data):
+        try:
+            msg = json.loads(data)
+            print(msg['text'].encode('utf-8'))
+            self.ws.write_message(msg['text'].encode('utf-8'))
+            return True
+        except BaseException as e:
+            return False
+
+    def on_error(self, status):
+        print(status)
+        return True
+# ---------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------
+# messa in ascolto del websocket server
+if __name__ == "__main__":
+    http_server = httpserver.HTTPServer(application)
+    port = 9000
+    http_server.listen(port)
+    myIP = '127.0.0.1'
+    print('*** Websocket Server in ascolto su '+myIP+':'+str(port)+'***')
+    ioloop.IOLoop.instance().start()
+# ---------------------------------------------------------------------------
